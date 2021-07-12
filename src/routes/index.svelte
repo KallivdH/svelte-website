6a61728fc4d65386c17bd3f8666f26857f90b5bb
Kalli van den Heuvel <Kallivdh@gmail.com>
2021-07-12 22:26:53 +0200

    form and color changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
             <h2 class="text-4xl mb-2">Welcome, I am</h2>
             <h1 class="text-6xl mb-5">Kalli van den Heuvel</h1>
             <p class="mb-5">I work as a programmer at Directlease in Henglo. Furthermore I study HBO-ICT at Saxion enschede. You can find interesting stuff about programming, climbing and music on this website. Soooo don't leave yet.</p>
-            <a class="bg-green-500 px-4 py-2" href="/#projects">Projects</a>
+            <a class="bg-green-700 px-4 py-2 text-white" href="/#projects">Projects</a>
         </div>
         <div id="hero-img">
             <img class="float-left md:float-right" src="kalli.png" alt="Kalli van den Heuvel" width="383px" height="403.91px">
@@ -38,37 +38,37 @@
         <h1 class="text-6xl mb-5">Contact me</h1>
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="text-xs font-bold mb-2" for="grid-first-name">
+            <label name="first-name" class="text-xs font-bold mb-2" for="first-name">
                 First Name
             </label>
-            <input class="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" required>
+            <input class="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" required>
             </div>
             <div class="w-full md:w-1/2 px-3">
-            <label class="block tracking-wide text-xs font-bold mb-2" for="grid-last-name">
+            <label name="last-name" class="block tracking-wide text-xs font-bold mb-2" for="last-name">
                 Last Name
             </label>
-            <input class="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" required>
+            <input class="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="last-name" type="text" required>
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
-            <label class="block tracking-wide text-xs font-bold mb-2" for="grid-password">
+            <label class="block tracking-wide text-xs font-bold mb-2" for="email">
                 E-mail
             </label>
-            <input class="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required>
+            <input name="email" class="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" required>
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
-            <label class="block tracking-wide text-xs font-bold mb-2" for="grid-password">
+            <label class="block tracking-wide text-xs font-bold mb-2" for="message">
                 Message
             </label>
-            <textarea class=" no-resize appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required></textarea>
+            <textarea name="message" class="no-resize appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" required></textarea>
             </div>
         </div>
         <div class="md:flex md:items-center">
             <div class="md:w-1/3">
-            <button class="bg-green-500 py-2 px-4" type="submit">
+            <button class="bg-green-700 text-white py-2 px-4" type="submit">
                 Send
             </button>
             </div>
