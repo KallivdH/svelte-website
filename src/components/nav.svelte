ca729486540a27c99912cdf4f7430623708f5ad0
Kalli van den Heuvel <Kallivdh@gmail.com>
2021-07-13 20:59:54 +0200

    fixed responsiveness

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,7 +1,88 @@
-<nav class="p-5">
+<!-- <nav class="p-5">
 	<div class="max-w-6xl mx-auto text-white">
-		<a class="bg-green-700 px-4 py-2 rounded-3xl mr-5 float-left" href="/">Kalli van den Heuvel</a>
+		<a class="bg-blue-700 px-4 py-2 rounded-3xl mr-5 float-left" href="/">Kalli van den Heuvel</a>
 		<a class="bg-red-700 px-4 py-2 rounded-3xl float-right" href="/contact">Contact</a>
-		<a class="bg-blue-700 px-4 py-2 rounded-3xl mr-5 float-right" href="/#projects">Projects</a>
+		<a class="bg-green-700 px-4 py-2 rounded-3xl mr-5 float-right" href="/#projects">Projects</a>
 	</div>
-</nav>
+</nav> -->
+<script>
+	import { onMount } from 'svelte';
+	let mobileMenuOpen = false;
+
+	function clickOutside(node, { enabled: initialEnabled, cb }) {
+		const handleOutsideClick = ({ target }) => {
+			if (!node.contains(target)) {
+				cb();
+			}
+		};
+
+		function update({ enabled }) {
+			if (enabled) {
+				window.addEventListener('click', handleOutsideClick);
+			} else {
+				window.removeEventListener('click', handleOutsideClick);
+			}
+		}
+
+		update({ enabled: initialEnabled });
+
+		return {
+			update,
+			destroy() {
+				window.removeEventListener('click', handleOutsideClick);
+			}
+		};
+	}
+
+	const mediaQueryHandler = (e) => {
+		if (!e.matches) {
+			mobileMenuOpen = false;
+		}
+	};
+
+	onMount(() => {
+		const mediaListener = window.matchMedia('(max-width: 767px)');
+		mediaListener.addListener(mediaQueryHandler);
+	});
+</script>
+
+<header
+	use:clickOutside={{ enabled: mobileMenuOpen, cb: () => (mobileMenuOpen = false) }}
+	class="max-w-6xl mx-auto flex flex-wrap flex-row justify-between md:items-center md:space-x-4 py-6 px-6 relative text-white"
+>
+	<a on:click={() => (mobileMenuOpen = false)} class="bg-blue-700 px-4 py-2 rounded-3xl" href="/"
+		>Kalli van den Heuvel</a
+	>
+	<button
+		on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
+		class="inline-block md:hidden w-10 h-10 p-2 bg-blue-700 rounded-3xl"
+	>
+		<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill-rule="evenodd"
+				d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+				clip-rule="evenodd"
+			/></svg
+		>
+	</button>
+	<nav
+		class="{mobileMenuOpen
+			? 'flex'
+			: 'hidden'} absolute md:relative top-16 left-0 md:top-0 z-20 md:flex flex-col md:flex-row md:space-x-6 space-y-5 md:space-y-0 w-full md:w-auto bg-gray-100 shadow-md md:shadow-none md:bg-transparent p-6 pt-4 md:p-0 "
+	>
+		<div>
+			<a
+				on:click={() => (mobileMenuOpen = false)}
+				class="bg-red-700 px-4 py-2 rounded-3xl"
+				href="/contact">Contact</a
+			>
+		</div>
+		<div>
+			<a
+				on:click={() => (mobileMenuOpen = false)}
+				class="bg-green-700 px-4 py-2 rounded-3xl mr-5"
+				href="/#projects">Projects</a
+			>
+		</div>
+	</nav>
+</header>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,25 +10,33 @@
 			</p>
 			<a class="bg-green-700 px-4 py-2 text-white" href="/#projects">Projects</a>
 		</div>
-		<div id="hero-img">
+		<div class="mt-5 md:mt-0" id="hero-img">
 			<img
 				class="float-left md:float-right"
-				src="kalli.png"
+				src="kalli.png?nf_resize=fit&w=383&h=404"
 				alt="Kalli van den Heuvel"
 				width="383px"
-				height="403.91px"
+				height="404px"
 			/>
 		</div>
 	</div>
 </div>
 <div id="projects" class="text-white font-mono py-10">
-	<div class="w-full bg-blue-700 p-10 rounded-3xl flex flex-wrap md:flex-nowrap mb-10">
-		<div class="p-10">
-			<img src="website-stack.png" alt="Website stack" width="1000px" height="1000px" />
+	<div
+		class="w-full bg-blue-700 md:p-5 lg:p-10 rounded-3xl flex flex-wrap justify-center md:flex-nowrap mb-10"
+	>
+		<div class="flex-shrink-0 p-6 sm:p-10">
+			<img
+				class="object-contain"
+				src="website-stack.png?nf_resize=fit&w=250&h=226"
+				alt="Website stack"
+				width="250px"
+				height="250px"
+			/>
 		</div>
-		<div class="p-10">
+		<div class="p-6 sm:p-10">
 			<h2 class="text-4xl mb-2">This website</h2>
-			<p class="mb-5">
+			<p class="mb-5 text-xs sm:text-base">
 				I have chosen to make this website with svelte. Svelte is a frontend framework that doesn't
 				use a Virtual DOM. There for its pretty fast and I like the coding workflow a lot. Further I
 				use tailwindCSS, this is a utility based css libary. The hosting is done on netlify where
@@ -40,25 +48,27 @@
 			>
 		</div>
 	</div>
-	<div class="w-full bg-red-700 p-10 rounded-3xl flex flex-wrap md:flex-nowrap">
-		<div class="p-10">
+	<div
+		class="w-full bg-red-700 md:p-5 lg:p-10 rounded-3xl flex justify-center flex-wrap md:flex-nowrap"
+	>
+		<div class="p-6 sm:p-10">
 			<h2 class="text-4xl mb-2">Spring boot powered website</h2>
-			<p class="mb-5">
+			<p class="mb-5 text-xs sm:text-base">
 				This is a school assignment I had to do. The front end is vanilla javascript and every api
-				call is done with fetch. Further is the backend made with spring boot.
+				call is done with fetch. Furthermore is the backend made with spring boot.
 			</p>
 			<a
 				class="bg-gray-100 px-4 py-2 text-red-700"
 				href="https://github.com/KallivdH/svelte-website">Github</a
 			>
 		</div>
-		<div class="p-10">
+		<div class="p-6 sm:p-10 flex-shrink-0">
 			<img
-				src="spring-boot.png"
-				class="float-right"
+				src="spring-boot.png?nf_resize=fit&w=200&h=180"
+				class="float-right object-contain"
 				alt="Website stack"
-				width="1000px"
-				height="1000px"
+				width="200px"
+				height="200px"
 			/>
 		</div>
 	</div>
