af4ad381c9fa40656923c1a2fbce12d9792924f6
Kalli van den Heuvel <Kallivdh@gmail.com>
2021-07-16 13:19:33 +0200

    added aria-label to mobile menu button

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -56,14 +56,15 @@
 	<button
 		on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
 		class="inline-block md:hidden w-10 h-10 p-2 bg-blue-700 rounded-3xl"
+		aria-label="Mobile menu"
 	>
-		<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
-			><path
+		<svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+			<path
 				fill-rule="evenodd"
 				d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
 				clip-rule="evenodd"
-			/></svg
-		>
+			/>
+		</svg>
 	</button>
 	<nav
 		class="{mobileMenuOpen
